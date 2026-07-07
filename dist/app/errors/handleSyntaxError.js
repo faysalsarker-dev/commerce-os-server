ff74faf840dd4db0e710d4d8e0a63b62c6ee345e
faysalsarker-dev <faysalsarker.dev@gmail.com>
2026-07-07 09:38:46 +0600

    add: local env

--- dist/app/errors/handleSyntaxError.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleSyntaxError = void 0;
-const ApiError_1 = require("./ApiError");
-const handleSyntaxError = () => new ApiError_1.AppError("Invalid JSON in request body. Please check your request format.", 400);
-exports.handleSyntaxError = handleSyntaxError;
--- dist/app/lib/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const adapter_pg_1 = require("@prisma/adapter-pg");
-const extension_1 = require("@prisma/client/extension");
-require("dotenv/config");
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new adapter_pg_1.PrismaPg({ connectionString });
-const prisma = new extension_1.PrismaClient({ adapter });
-exports.default = prisma;
